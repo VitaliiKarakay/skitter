aa6edf186b7154740c1d978c8ce52d4d444d04c1
Vitaliy Karakay <iipizpak@gmail.com>
2022-04-26 17:36:50 +0300

    added fileUploading and sharing

--- src/main/java/com/vkarakay/sweater/controller/UserController.java
+++ src/main/java/com/vkarakay/sweater/controller/UserController.java
@@ -4,6 +4,7 @@ import com.vkarakay.sweater.domain.Role;
 import com.vkarakay.sweater.domain.User;
 import com.vkarakay.sweater.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
