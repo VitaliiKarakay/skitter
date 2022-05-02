72fc4649549f89dce95ba93933349d419838133f
Vitaliy Karakay <iipizpak@gmail.com>
2022-05-02 21:31:25 +0300

    Edit message, user's messages

--- src/main/java/com/vkarakay/sweater/controller/MainController.java
+++ src/main/java/com/vkarakay/sweater/controller/MainController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.ObjectUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,6 +20,7 @@ import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
 @Controller
@@ -29,7 +32,7 @@ public class MainController {
     private String uploadPath;
 
     @GetMapping("/")
-    public String greeting(Map<String, Object> model) {
+    public String greeting(Model model) {
         return "greeting";
     }
 
@@ -53,7 +56,7 @@ public class MainController {
             @Valid Message message,
             BindingResult bindingResult,
             @RequestParam("file") MultipartFile file,
-            Model model)  {
+            Model model) {
         message.setAuthor(user);
         if (bindingResult.hasErrors()) {
             Map<String, String> errorsMap = ControllerUtils.getErrors(bindingResult);
@@ -61,21 +64,10 @@ public class MainController {
             model.addAttribute("message", message);
         } else {
 
-            if (file != null && !file.getOriginalFilename().isEmpty()) {
-                File uploadFolder = new File(uploadPath);
-
-                if (!uploadFolder.exists()) {
-                    uploadFolder.mkdir();
-                }
-                String uuidFile = UUID.randomUUID().toString();
-                String resultFilename = uuidFile + "." + file.getOriginalFilename();
-
-                try {
-                    file.transferTo(new File(uploadPath + "/" + resultFilename));
-                } catch (IOException e) {
-                    model.addAttribute("message","File not found");
-                }
-                message.setFilename(resultFilename);
+            try {
+                saveFile(message, file);
+            } catch (IOException e) {
+                model.addAttribute("message", "File not found");
             }
             model.addAttribute("message", null);
             messageRepository.save(message);
@@ -83,5 +75,62 @@ public class MainController {
         Iterable<Message> allMessages = messageRepository.findAll();
         model.addAttribute("messages", allMessages);
         return "main";
+    }
+
+    private void saveFile(Message message, MultipartFile file) throws IOException {
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadFolder = new File(uploadPath);
+
+            if (!uploadFolder.exists()) {
+                uploadFolder.mkdir();
+            }
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            message.setFilename(resultFilename);
+        }
+    }
+
+    @GetMapping("/user-messages/{user}")
+    public String userMessages(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable User user,
+            Model model,
+            @RequestParam(required = false) Message message
+    ) {
+        Set<Message> messages = user.getMessages();
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("message", message);
+        model.addAttribute("isCurrentUser", currentUser.equals(user));
+
+        return "userMessages";
+    }
+
+    @PostMapping("/user-messages/{user}")
+    public String updateMessage(
+            @AuthenticationPrincipal User currentUser,
+            @PathVariable Long user,
+            @RequestParam("id") Message message,
+            @RequestParam("text") String text,
+            @RequestParam("tag") String tag,
+            @RequestParam("file") MultipartFile file
+    ) throws IOException {
+        if (message.getAuthor().equals(currentUser)) {
+            if (!ObjectUtils.isEmpty(text)) {
+                message.setText(text);
+            }
+
+            if (!ObjectUtils.isEmpty(tag)) {
+                message.setTag(tag);
+            }
+
+            saveFile(message, file);
+
+            messageRepository.save(message);
+        }
+
+        return "redirect:/user-messages/" + user;
     }
 }
